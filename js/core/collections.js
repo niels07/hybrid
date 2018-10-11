ae8c692677a9e4219cedc1c79d7a1b3f745f0e2f
Niels Vanden Eynde <niels.vandeneynde@hotmail.com>
2018-10-11 17:17:48 +0200

    removed compiled js files

--- js/core/collections.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports", "./collections/genericCollectionBase"], function (require, exports, genericCollectionBase_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.GenericCollectionBase = genericCollectionBase_1.GenericCollectionBase;
-});
-//# sourceMappingURL=collections.js.map
